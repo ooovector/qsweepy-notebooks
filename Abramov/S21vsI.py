a3129a1ee882bea5a78b834ec348cf1f9670f822
GrigorevSasha <grigorev.sasha8008@gmail.com>
2019-10-30 12:09:55 +0300

    Changes after 24 qubit measument and control experiment

--- Abramov/S21vsI.py
+++ Abramov/S21vsI.py
@@ -1,28 +1,24 @@
+from qsweepy import *
+from qsweepy.instruments import *
+
 from numpy import *
 import sys
-qt_dir = "D:/qtlab_replacement"
-sys.path.append(qt_dir)
-import init
-from instruments import *
-import sweep
 import time
-import save_pkl
 
 #Data mamagement
-Data_dir ="D:/Abramov/data" 
-path = save_pkl.mk_dir(root = Data_dir)
+Data_dir ="C:/Users/cryopxi/Documents/Abramov/data" 
+path = save_pkl.default_measurement_save_path(root = Data_dir, mkdir=True)
 print("Saving files to: ",path)
 
 currents = arange(-1.,1, 0.02)*1e-3
 
 pna_preset = True
 
-cs = Yokogawa_GS210(address='GPIB0::2::INSTR')
-pna = Agilent_N5242A('pna', address = 'pna')
+cs = Yokogawa_GS210(address='GPIB1::1::INSTR')
+pna = Agilent_N5242A('pna', address = 'ZVB20-23-100170')
 cs.set_range(0.01)
 if pna_preset:
-	pna.set_nop(500)
-	pna.set_power(-50.)
+   	pna.set_power(-50.)
 	pna.set_bandwidth(400)
 	pna.set_centerfreq(6.87656e9)
 	pna.set_span(100e6)
